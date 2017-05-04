bb536f24494e8d643cf4c3b368cffb9cf332ec89
Joseph Chen <j.joseph.chen@berkeley.edu>
2017-05-03 22:32:33 -0700

    Completed Demo, uploading Assets

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CameraController : MonoBehaviour {
+
+	public Light flashlight;
+
+	private float speed = 100.0F;
+
+	// Use this for initialization
+	void Start () {
+		Input.gyro.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion rotation = getDeviceOrientation ();
+
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) {
+			Debug.Log (rotation.ToString ());
+		}
+
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Stationary) {
+			GetComponent<Transform> ().Translate (speed * Vector3.forward * Time.deltaTime, Camera.main.transform);
+			flashlight.GetComponent<Transform> ().position = GetComponent<Transform> ().position;
+		}
+
+		GetComponent<Transform> ().rotation = rotation;
+		flashlight.GetComponent<Transform> ().rotation = rotation;
+	}
+
+	Quaternion getDeviceOrientation () {
+		Quaternion gyro = Input.gyro.attitude;
+		Quaternion rotation = new Quaternion (gyro.x, gyro.y, -gyro.z, -gyro.w); // Unity is left handed, Gyroscope is right handed
+
+		return rotation;
+	}
+}
---
+++ Assets/Scripts/RotateCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour {
+
+	private Vector3 rotateVector = new Vector3(10.0F, 20.0F, 40.0F);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Transform> ().Rotate(rotateVector * Time.deltaTime);
+	}
+}
